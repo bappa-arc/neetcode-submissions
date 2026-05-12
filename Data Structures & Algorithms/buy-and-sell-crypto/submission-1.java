ba6b54877213372e8d2674650c55b887e4602852
bappa-arc <dasbappa7063378859@gmail.com>
2026-05-13 00:11:03 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for(int i=0;i<prices.length-1;i++){
+            for(int j=i+1; j<prices.length;j++){
+                int currentProfit=prices[j]-prices[i];
+                if(currentProfit>maxProfit){
+                    maxProfit=currentProfit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
