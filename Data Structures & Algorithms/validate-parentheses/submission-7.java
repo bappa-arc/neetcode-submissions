5f3646df89c0ffac3c944ca62c5fc318b0851b9e
bappa-arc <dasbappa7063378859@gmail.com>
2026-06-06 01:03:21 +0530

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length()%2 != 0) return false;
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()){
+            if (c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else {
+                if (stack.empty()) return false;
+                if ((c == ')' && stack.peek() == '(') || (c == '}' && stack.peek() == '{') || (c == ']' && stack.peek() == '[')){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
